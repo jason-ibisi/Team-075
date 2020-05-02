1d6f0377d2a4eb17dd9afe895af08eacc4238741
Bee <ardelmbiplang@gmail.com>
2020-05-02 18:04:16 +0100

    feat: Compute the closest Response unit to victim - corrected typo

--- backend/controllers/responseController.js
+++ backend/controllers/responseController.js
@@ -2,16 +2,16 @@ const geolib = require('geolib');
 
 
 
-const victimsLocation = { latitude: 52.456221, longitude: 12.63128 } //Victims loaction received from the front-end
+// const victimsLocation = { latitude: 52.456221, longitude: 12.63128 } //Victims loaction received from the front-end
 
-const emergencyResponseLocation =  [ // All the Emergency Response units in that location (state) in an array.
-    { latitude: 52.516272, longitude: 13.377722 },
-    { latitude: 51.515, longitude: 7.453619 },
-    { latitude: 51.503333, longitude: -0.119722 },
-    { latitude: 55.751667, longitude: 37.617778 },
-    { latitude: 48.8583, longitude: 2.2945 },
-    { latitude: 59.3275, longitude: 18.0675 },
-    { latitude: 59.916911, longitude: 10.727567 },
+// const emergencyResponseLocation =  [ // All the Emergency Response units in that location (state) in an array.
+//     { latitude: 52.516272, longitude: 13.377722 },
+//     { latitude: 51.515, longitude: 7.453619 },
+//     { latitude: 51.503333, longitude: -0.119722 },
+//     { latitude: 55.751667, longitude: 37.617778 },
+//     { latitude: 48.8583, longitude: 2.2945 },
+//     { latitude: 59.3275, longitude: 18.0675 },
+//     { latitude: 59.916911, longitude: 10.727567 },
 ]
 
 
