46f2eab5a45db9e579325378445774b25a32756c
Bee <ardelmbiplang@gmail.com>
2020-05-03 18:48:49 +0100

    refact: Tested for different inputs for response controller

--- backend/controllers/responseController.js
+++ backend/controllers/responseController.js
@@ -1,5 +1,16 @@
 const geolib = require('geolib');
 
-const victimAndResponseUnit = (victimsCoordinate, emergencyResponseCoordinates) => `${geolib.getPreciseDistance(victimsCoordinate, geolib.findNearest(victimsCoordinate, emergencyResponseCoordinates)) / 1000}km`;
+const victimAndResponseUnit = (victimsCoordinate, emergencyResponseCoordinates) => {
+  if (!victimsCoordinate || !emergencyResponseCoordinates) {
+    return 'Invalid input to victimAndResponseUnit.';
+  }
+  if (typeof (victimsCoordinate) !== 'object' || typeof (emergencyResponseCoordinates) !== 'object') {
+    return 'Invalid input to victimAndResponseUnit.';
+  }
+  if (!emergencyResponseCoordinates.length) {
+    return 'Invalid input to victimAndResponseUnit.';
+  }
+  return `${geolib.getPreciseDistance(victimsCoordinate, geolib.findNearest(victimsCoordinate, emergencyResponseCoordinates)) / 1000}km`;
+};
 
 module.exports = victimAndResponseUnit;
